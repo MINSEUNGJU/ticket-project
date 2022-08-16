80526ccb866f7454c71fda507542b67d0dfb2412
seungju <jooyful14@gmail.com>
2022-08-16 16:01:39 +0900

    수정

---
+++ ticket/src/main/java/com/ticket/domain/ReservationDetailDTO.java
@@ -0,0 +1,16 @@
+package com.ticket.domain;
+
+public class ReservationDetailDTO {
+	private int no;
+	private String userId;
+	private String movieTitle;
+	private String moviePoster;
+	private int movieAge;
+	private String cinemaName;
+	private String date;
+	private String theaterNo;
+	private String seatNo;
+	private String price;
+	private String payType;
+
+}
---
+++ ticket/src/main/java/com/ticket/domain/ScreeningFilmDTO.java
@@ -0,0 +1,12 @@
+package com.ticket.domain;
+
+public class ScreeningFilmDTO {
+	private int no;
+	private int cinemaNo;
+	private int movieNo;
+	private String time;
+	private int month;
+	private int day;
+	private String yoil;
+
+}
