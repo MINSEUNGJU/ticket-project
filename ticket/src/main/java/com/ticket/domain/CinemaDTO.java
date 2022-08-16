de972c6a8487347e99557a51946e6c44508d30f6
HJ31034 <h890805@gmail.com>
2022-08-16 16:05:34 +0900

    DTOupdate_HJ
    DTOupdate_HJ

---
+++ ticket/src/main/java/com/ticket/domain/CinemaDTO.java
@@ -0,0 +1,17 @@
+package com.ticket.domain;
+
+import lombok.Data;
+
+@Data
+public class CinemaDTO {
+	private String code;
+	private String name;
+	private String address;
+	private int latitude ;
+	private int longitude;
+	private String Sido;
+}
+
+
+
+
---
+++ ticket/src/main/java/com/ticket/domain/MovieDTO.java
@@ -0,0 +1,27 @@
+package com.ticket.domain;
+
+import lombok.Data;
+
+@Data
+public class MovieDTO {
+	private int no;
+	private String title;
+	private String poster;
+	private String director;
+	private String actor ;
+	private String genre;
+	private int age;
+	private String regdate;
+	private String rate;
+	 
+	
+	
+	
+	 
+	 
+	
+ 
+
+
+
+}
